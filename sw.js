6b8d99755d93d1983b32aa422f6a214e844544d5
A. Aurelions <aaurelions@gmail.com>
2025-08-04 23:01:51 +0300

    Bookava v0.0.2

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "bookava-cache-v3"; // Cache version incremented
+const urlsToCache = [
+  "/",
+  "index.html",
+  // Note: The following external URLs are intentionally not cached
+  // as they can cause the service worker installation to fail if the
+  // resources are unavailable or return non-200 responses.
+  // It's generally better to let the browser handle caching for these.
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
